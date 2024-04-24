15b89404b5a9dbaeca979ed3b93ec5fb87b18a02
Angelica <angelicaprajoo@gmail.com>
2024-04-24 17:50:21 +0100

    primeirocommit

---
+++ app/src/main/java/com/example/meuprimeiroprojeto/ResultActivity.kt
@@ -0,0 +1,52 @@
+package com.example.meuprimeiroprojeto
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tvResult = findViewById<TextView>(R.id.textview_result)
+        val tvClassficacao = findViewById<TextView>(R.id.textview_classifacacao)
+        val result = intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+
+        tvResult.text = result.toString()
+
+
+        val classificacao = if (result < 18.5f) {
+            "ABAIXO DO PESO"
+        } else if (result in 18.5f..24.9f) {
+            "NORMAL"
+        } else if (result in 25f..29.9f) {
+            "SOBREPESO"
+        } else if (result in 30f..39.9f) {
+            "OBESIDADE"
+        } else {
+            "OBESIDADE GRAVE"
+        }
+
+        tvClassficacao.text = getString(R.string.message_classificaçao, classificacao)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+    }
+
+
